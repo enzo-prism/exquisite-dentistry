2682b905c3adc43b0ba8118fb6019f6b6c3dbd0b
enzo <enzo@design-prism.com>
2026-08-30 07:55:24 -0700

    fix: load blog overflow and FAQ wrap rules on every article

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './blog-layout.css';
 import App from './App.tsx';
 import { initializeUTMTracking } from './utils/utmTracking';
 import { initializeGoogleAdsTracking } from './utils/googleAdsTracking';
